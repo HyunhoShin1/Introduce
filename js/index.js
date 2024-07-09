04723594e8e9d3813f5a190798fa11cea4d37486
HyunhoShin1 <zxcv5338@naver.com>
2024-07-09 11:03:26 +0900

    123

--- js/index.js
+++ js/index.js
@@ -1,57 +1,110 @@
 $(document).ready(function () {
-    
-    $('.logo').click(function() {
-        $('html, body').animate({ scrollTop: 0 }, 'slow');
-        return false;
-      });
 
-    /* 모바일 메뉴 버튼*/
-    $('.mobile-menu-bar').on('click', function() {
-        $(this).toggleClass('active');
+  var $html = $("html, body");
+    var page = 1;
+    var lastPage = $("section").length;
+    var isScrolling = false;
+
+    $html.scrollTop(0);
+
+    function closeDoors(callback) {
+        const doors = $('.elevator-doors');
+        doors.addClass('doors-closing');
+        setTimeout(function() {
+            if (callback) callback();
+            doors.removeClass('doors-closing');
+        }, 1000);
+    }
+
+    function scrollToSection(index) {
+        if (isScrolling || index < 1 || index > lastPage) return;
+
+        isScrolling = true;
+        page = index;
+        var posTop = (page - 1) * $(window).height();
+        closeDoors(function() {
+            $html.scrollTop(posTop);
+            isScrolling = false;
+        });
+    }
+
+    $(window).on("wheel", function(e) {
+        if (isScrolling) return;
+        e.preventDefault();
+
+        if (e.originalEvent.deltaY > 0) {
+            if (page == lastPage) return;
+            page++;
+        } else if (e.originalEvent.deltaY < 0) {
+            if (page == 1) return;
+            page--;
+        }
+
+        scrollToSection(page);
     });
 
+    $('.gnb li').on('click', function() {
+        const index = $(this).index() + 1; // +1 to start from 1
+        scrollToSection(index);
+    });
 
-    /* 스킬 탭버튼 */
-    $('#toggle-on').on('click', tabmenu1);
-    function tabmenu1() {
-        $('.languages').show();
-        $('.tools').hide();
-    }
+  /* 로고 클릭 */
+  $(".logo").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
+  });
 
-    $('#toggle-off').on('click', tabmenu2);
-    function tabmenu2() {
-        $('.languages').hide();
-        $('.tools').show();
-    }
+  /* 모바일 메뉴 버튼*/
+  $(".mobile-menu-bar").on("click", function () {
+    $(this).toggleClass("active");
+  });
 
+  /* 스킬 탭버튼 */
+  $("#toggle-on").on("click", tabmenu1);
+  function tabmenu1() {
+    $(".languages").show();
+    $(".tools").hide();
+  }
 
-    /* 스킬 툴팁 */
-    $('.skill-icon').mouseenter(function () {
-        const toolname = $(this).data('name');
-        const widthPercentage = $(this).data('width');
-        const text = $(this).data('text');
-        const tooltip = $('.tooltip');
-    
-        tooltip.show();
-        
-        $('.toolname').text(toolname);
-    
-        $('.loading-bar').css('width', '0%').animate({
-          width: widthPercentage + '%'
-        }, {
-          duration: 200,
-          easing: 'linear',
-          step: function(now) {
-            $('.per-text').text(Math.round(now) + '%');
+  $("#toggle-off").on("click", tabmenu2);
+  function tabmenu2() {
+    $(".languages").hide();
+    $(".tools").show();
+  }
+
+  /* 스킬 툴팁 */
+  $(".skill-icon")
+    .mouseenter(function () {
+      const toolname = $(this).data("name");
+      const widthPercentage = $(this).data("width");
+      const text = $(this).data("text");
+      const tooltip = $(".tooltip");
+
+      tooltip.show();
+
+      $(".toolname").text(toolname);
+
+      $(".loading-bar")
+        .css("width", "0%")
+        .animate(
+          {
+            width: widthPercentage + "%",
+          },
+          {
+            duration: 200,
+            easing: "linear",
+            step: function (now) {
+              $(".per-text").text(Math.round(now) + "%");
+            },
           }
-        });
-    
-        $('.tool-text').text(text);
-      }).mouseleave(function() {
-        const tooltip = $('.tooltip');
-        tooltip.hide();
-        $('.loading-bar').stop().css('width', '0%');
-        $('.per-text').text('0%');
-      });
+        );
 
+      $(".tool-text").text(text);
+    })
+    .mouseleave(function () {
+      const tooltip = $(".tooltip");
+      tooltip.hide();
+      $(".loading-bar").stop().css("width", "0%");
+      $(".per-text").text("0%");
+    });
 });
